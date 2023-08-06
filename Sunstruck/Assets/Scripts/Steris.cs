b5824bd27b6dcfd13c40c67abe2ba38fa59dab2e
ChongSiuHin <107177861+ChongSiuHin@users.noreply.github.com>
2023-08-06 17:03:25 +0800

    Animator bug fix,menu fix alr, setting UI update, Suit bar Update,walking animator fix, Sister i make a new script.

--- Sunstruck/Assets/Scripts/Steris.cs
+++ Sunstruck/Assets/Scripts/Steris.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class Steris : MonoBehaviour
 {
     [SerializeField] private GameObject theoby;
-    private float lastPosition;
+    [SerializeField] private float followSpeed = 2f;
+    private Vector3 velocity = Vector3.zero;
+    //private float lastPosition;
     private BoxCollider2D selfCollider;
     private Animator anim;
+
+    //float startChasingDistance = 1.0f;
+    //float stopChasingDistance = 1.0f;
 
     void Start()
     {
@@ -19,17 +24,13 @@ public class Steris : MonoBehaviour
     {
         float distanceWithTheoby = transform.localPosition.x - theoby.transform.localPosition.x;
 
-        if (distanceWithTheoby < -1 && theoby.transform.localScale.x == 1)
+        if (Mathf.Abs(distanceWithTheoby) > 1)
         {
             anim.SetBool("Walking", true);
-            transform.localScale = new Vector2(1, 1);
-            transform.position = Vector2.MoveTowards(transform.position, theoby.transform.position, theoby.GetComponent<Rigidbody2D>().velocity.x * Time.deltaTime);
-        }
-        else if (distanceWithTheoby > 1 && theoby.transform.localScale.x == -1)
-        {
-            anim.SetBool("Walking", true);
-            transform.localScale = new Vector2(-1, 1);
-            transform.position = Vector2.MoveTowards(transform.position, theoby.transform.position, -theoby.GetComponent<Rigidbody2D>().velocity.x * Time.deltaTime);
+            Vector3 targetPosition = new Vector3(theoby.transform.position.x, transform.position.y, transform.position.z);
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, followSpeed);
+
+            transform.localScale = new Vector2(distanceWithTheoby > 0 ? -1 : 1, 1);
         }
         else
         {
@@ -37,5 +38,29 @@ public class Steris : MonoBehaviour
         }
 
         Physics2D.IgnoreCollision(selfCollider, theoby.GetComponent<BoxCollider2D>());
-    }    
+    }
+
+    //void Update()
+    //{
+    //    float distanceWithTheoby = transform.localPosition.x - theoby.transform.localPosition.x;
+
+    //    if (distanceWithTheoby < -startChasingDistance && theoby.transform.localScale.x == 1)
+    //    {
+    //        anim.SetBool("Walking", true);
+    //        transform.localScale = new Vector2(1, 1);
+    //        transform.position = Vector2.MoveTowards(transform.position, theoby.transform.position, theoby.GetComponent<Rigidbody2D>().velocity.x * Time.deltaTime);
+    //    }
+    //    else if (distanceWithTheoby > startChasingDistance && theoby.transform.localScale.x == -1)
+    //    {
+    //        anim.SetBool("Walking", true);
+    //        transform.localScale = new Vector2(-1, 1);
+    //        transform.position = Vector2.MoveTowards(transform.position, theoby.transform.position, -theoby.GetComponent<Rigidbody2D>().velocity.x * Time.deltaTime);
+    //    }
+    //    else if (distanceWithTheoby > -stopChasingDistance && distanceWithTheoby < stopChasingDistance)
+    //    {
+    //            anim.SetBool("Walking", false);
+    //    }
+
+    //    Physics2D.IgnoreCollision(selfCollider, theoby.GetComponent<BoxCollider2D>());
+    //}    
 }
